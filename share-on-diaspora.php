917de32eaf94468cedd4af3123448585a318d084
Vitalie Ciubotaru <Vitalie Ciubotaru@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-14 17:39:01 +0000

    tagging version 0.6.2
    git-svn-id: https://plugins.svn.wordpress.org/share-on-diaspora/trunk@1090001 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- share-on-diaspora.php
+++ share-on-diaspora.php
@@ -3,7 +3,7 @@
 Plugin Name: Share on Diaspora
 Plugin URI: https://github.com/ciubotaru/share-on-diaspora
 Description: This plugin adds a "Share on D*" button at the bottom of your posts.
-Version: 0.6.1
+Version: 0.6.2
 Author: Vitalie Ciubotaru
 Author URI: https://github.com/ciubotaru
 License: GPL2
@@ -25,18 +25,18 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'SHARE_ON_DIASPORA_VERSION', '0.6.1' );
+define( 'SHARE_ON_DIASPORA_VERSION', '0.6.2' );
 define( 'SHARE_ON_DIASPORA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SHARE_ON_DIASPORA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 register_activation_hook( __FILE__, array( 'ShareOnDiaspora', 'plugin_activation' ) );
-register_deactivation_hook( __FILE__, array( 'ShareOnDiaspora', 'plugin_deactivation' ) );
+register_uninstall_hook( __FILE__, array( 'ShareOnDiaspora', 'plugin_uninstall' ) );
 
 require_once( SHARE_ON_DIASPORA_PLUGIN_DIR . 'class.php' );
 if ( is_admin() ) {
 	require_once( SHARE_ON_DIASPORA_PLUGIN_DIR . 'admin.php' );
-        $shareondiaspora_admin = new ShareOnDiaspora_Admin;
-//	add_action( 'init', array( 'ShareOnDiaspora_Admin', '__construct' ) );
+		$shareondiaspora_admin = new ShareOnDiaspora_Admin;
+	//  add_action( 'init', array( 'ShareOnDiaspora_Admin', '__construct' ) );
 }
 
 $shareondiaspora = new ShareOnDiaspora;
